fd442bc00398fb2ccd24e7cffcca9d2a8bdc2143
VasilkovaPD <pvasilkova@yandex.ru>
2015-09-18 11:33:06 +0400

    2.5.1

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     cout << "Input A and B: ";
     int a,b;
     cin >>a >>b;
-    cout << "Sum A and B is: " <<(a+b);
+    cout << "Sum A and B is: " <<(a+b) <<"\n";
+    cout << "Dif A and B is: " <<(a-b);
 }
